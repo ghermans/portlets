d8f1d857720dc9f23c8390694695b83fdfcbd5b1
Nikola Bodrozic <nb@example.com>
2014-03-30 23:05:50 +0200

    added js/jquery.min.js

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ print_portlets('right');
 		</div>
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="js/bootstrap.min.js"></script>
 		<!-- jQuery UI customized JavaScript -->
