5c4ab4c174da4d94a2ea5d288e83d2362c52298c
Nikola Bodrozic <nb@example.com>
2014-03-30 11:50:08 +0200

    from index.html moved styles to css/main.css and JS to js/support

---
+++ js/support.js
@@ -0,0 +1,48 @@
+			var byColumns = "";
+			$(function() {
+				$(".column").sortable({
+					connectWith : ".column"
+				});
+
+				$(".portlet-header .ui-icon").click(function() {
+					$(this).toggleClass("ui-icon-minusthick").toggleClass("ui-icon-plusthick");
+					$(this).parents(".portlet:first").find(".portlet-content").toggle();
+					var sign = $(this).hasClass("ui-icon-plusthick") ? "plus" : "minus";
+					var portlet_id = $(this).parent().parent().attr('id');
+					$('#info_cols').html('?sendSigns=yes&portlet_id=' + portlet_id + '&sign=' + sign);
+
+					//$.get("ajax.php"+'?sendSigns=yes&portlet_id='+portlet_id+'&sign='+sign, function(data){ $('#so').text(data);} );
+				});
+
+				$(".column").disableSelection();
+
+				$(".column").on("sortstop", function(event, ui) {
+
+					var i = 0;
+					$("#left > div").each(function() {
+						byColumns += '&left[' + i + ']=' + $(this).attr('id');
+						i++;
+					});
+
+					var i = 0;
+					$("#middle > div").each(function() {
+						byColumns += '&middle[' + i + ']=' + $(this).attr('id');
+						i++;
+					});
+
+					var i = 0;
+					$("#right > div").each(function() {
+						byColumns += '&right[' + i + ']=' + $(this).attr('id');
+						i++;
+					});
+
+					byColumns = '?sendColumns=yes' + byColumns;
+
+					//$.get("ajax.php"+byColumns, function(data){ $('#so').text(data);} );
+
+					$('#info_cols').html(byColumns);
+
+					byColumns = "";
+				});
+
+			});
