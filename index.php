81f028d52568ed29926bc2321a69053a350d9011
Nikola Bodrozic <nb@example.com>
2014-06-28 21:38:35 +0200

    modified index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 			}
 		</style>
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesnt work if you view the page via file:// -->
+		<!-- WARNING: Respond.js does not work if you view the page via file:// -->
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
